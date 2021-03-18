44aad084eb0a18d14c074447e11154d2bdb5b921
[junmar0117] <[marjungil17@gmail.com]>
2021-03-18 14:16:14 +0800

    add connection and checkLogin.php

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+$db_name = "thesisdb";
+$db_username = "root";
+$db_pass = "Passw0rd";
+$db_host = "localhost";
+
+$con = mysqli_connect("$db_host","$db_username","$db_pass", "$db_name") or die(mysqli_connect_error()); //Connect to server
+?>
