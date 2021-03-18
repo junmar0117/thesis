8b7baeed22c067e4f9c0211365783bb332f97fcb
dellyy <dell.jardin.maranan@gmail.com>
2021-03-18 22:49:57 +0800

    footer, login, signup, index

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@
 <html lang="en">
 <head>
     <meta charset = "utf-8">
-    <title> Login Page </title>
+    <title> Sign Up </title>
     <meta name ="viewport" content="width=devoce-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/styles.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
@@ -27,8 +27,29 @@
     </nav>
 
     <div class="login-box">
-        <img src="login.ico" style="float:left" width="80px" height="100px" >
-        <h1>Login</h1>
+        <img src="signup.png" style="float:left" width="80px" height="100px">
+        <h1>Sign Up</h1>
+
+        <div class="textbox">
+            <i class="fas fa-info-circle"></i>
+            <input type="text" placeholder="Full Name" name="" value="">
+        </div>
+
+        <div class="textbox">
+            <i class="fas fa-info-circle"></i>
+            <input type="number" placeholder="Age" name="" value="">
+        </div>
+
+        <div class="textbox">
+            <i class="fas fa-map-marker"></i>
+            <input type="text" placeholder="Address" name="" value="">
+        </div>
+
+        <div class="textbox">
+            <i class="fas fa-envelope"></i>
+            <input type="email" placeholder="Email" name="" value="">
+        </div>
+
         <div class="textbox">
             <i class="fas fa-user"></i>
             <input type="text" placeholder="Username" name="" value="">
@@ -39,7 +60,7 @@
             <input type="password" placeholder="Password" name="" value="">
         </div>
 
-        <input class="btn" type="button" name="" value="Sign In">
+        <input class="btn" type="button" name="" value="Sign Up">
     </div>
 
     <section>
