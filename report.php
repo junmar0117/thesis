9933e5e13c2ca08ad07f2b44e0fa69b0ed282a56
dellyy <dell.jardin.maranan@gmail.com>
2021-03-23 18:34:31 +0800

    report

--- report.php
+++ report.php
@@ -29,18 +29,21 @@
         <section class="columns">
             <div class="column">
                 <img src="./assets/pnplogo.png" style="a" width="250px" height="300px" >
+                <h2>PNP</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <input class="btn2" type="button" name="" value="REPORT INCIDENT">
             </div>
 
             <div class="column">
                 <img src="./assets/bfplogo.png"  width="250px" height="300px" >
+                <h2>BFP</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <input class="btn2" type="button" name="" value="REPORT INCIDENT">
             </div>
 
             <div class="column">
                 <img src="./assets/baranggaylogo.png" width="250px" height="300px" >
+                <h2>Barangay</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <input class="btn2" type="button" name="" value="REPORT INCIDENT">
             </div>
