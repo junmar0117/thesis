36c44e8b267964aa2e3b02680fc054dc4c0a84a8
dellyy <dell.jardin.maranan@gmail.com>
2021-03-23 18:33:07 +0800

    index, login, report, signup, logos, pinalitan ko styles.ccs ng style.css

---
+++ report.php
@@ -0,0 +1,72 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset = "utf-8">
+    <title> Report </title>
+    <meta name ="viewport" content="width=devoce-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <label class="logo">R&R</label>
+        <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">HOME</a></li>
+        </ul>
+    </nav>
+
+    <div class="wrapper">
+        <section class="columns">
+            <div class="column">
+                <img src="./assets/pnplogo.png" style="a" width="250px" height="300px" >
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <input class="btn2" type="button" name="" value="REPORT INCIDENT">
+            </div>
+
+            <div class="column">
+                <img src="./assets/bfplogo.png"  width="250px" height="300px" >
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <input class="btn2" type="button" name="" value="REPORT INCIDENT">
+            </div>
+
+            <div class="column">
+                <img src="./assets/baranggaylogo.png" width="250px" height="300px" >
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <input class="btn2" type="button" name="" value="REPORT INCIDENT">
+            </div>
+
+
+        </section>
+    </div>
+
+    <div class="footer">
+            <div class="footer-section-about" id="first">
+                <p style="font-family: Courier; font-size: 35px; font-weight: bold;">ABOUT US</p>
+            </div>
+            <div class="footer-section-links" id="second">
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">HOME</a></li>
+            </ul>
+            </div>
+        <div class="footer-bottom">
+            &copy; Copyright © R&R 2021 Digital All Rights Reserved
+        </div>
+    </div>
+</body>
+</html>
+
+
