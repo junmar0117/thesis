1840c86883afaa043b323f6c1c88e0e15fcdb126
dellyy <dell.jardin.maranan@gmail.com>
2021-03-28 19:44:24 +0800

    report pnp bfp and brgy

---
+++ reportBFP.php
@@ -0,0 +1,52 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset = "utf-8">
+    <title> Home Page </title>
+    <meta name ="viewport" content="width=devoce-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <label class="logo">R&R</label>
+        <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">HOME</a></li>
+        </ul>
+    </nav>
+    <section>
+    
+    </section>
+
+    <div class="footer">
+            <div class="footer-section-about" id="first">
+                <p style="font-family: Courier; font-size: 35px; font-weight: bold;">ABOUT US</p>
+            </div>
+            <div class="footer-section-links" id="second">
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">HOME</a></li>
+            </ul>
+            </div>
+        <div class="footer-bottom">
+            &copy; Copyright © R&R 2021 Digital All Rights Reserved
+        </div>
+    </div>
+</body>
+</html>
+
+
+
