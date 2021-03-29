9e58dcf9ce73ac8a2b25028f6074a098c62ea6bc
[junmar0117] <[marjungil17@gmail.com]>
2021-03-29 18:58:55 +0800

    removed unnecessary shits

--- login.php
+++ login.php
@@ -1,5 +1,7 @@
 <?php
+include_once('header.html');
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,22 +11,8 @@
     <link rel="stylesheet" href="./css/style.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
-
 <body>
-    <nav>
-        <input type="checkbox" id="check">
-        <label for="check" class="checkbtn">
-            <i class="fas fa-bars"></i>
-        </label>
-        <label class="logo">R&R</label>
-        <ul>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">HOME</a></li>
-        </ul>
-    </nav>
+   
 
     <div class="login-box">
         <img src="./assets/login.ico" style="float:left" width="80px" height="100px" >
@@ -46,24 +34,8 @@
     
     </section>
 
-    <div class="footer">
-            <div class="footer-section-about" id="first">
-                <p style="font-family: Courier; font-size: 35px; font-weight: bold;">ABOUT US</p>
-            </div>
-            <div class="footer-section-links" id="second">
-            <ul>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">HOME</a></li>
-            </ul>
-            </div>
-        <div class="footer-bottom">
-            &copy; Copyright © R&R 2021 Digital All Rights Reserved
-        </div>
-    </div>
 </body>
 </html>
+
 
 
