c09dcac1e0e0d1cd92ec4bb901155928f80b865c
solarman123 <lrlaurena@gmail.com>
2021-03-29 22:46:50 +0800

    report main page edited (partial), new external css for report main page, added new pictures

--- report.php
+++ report.php
@@ -4,71 +4,53 @@
 <html lang="en">
 <head>
     <meta charset = "utf-8">
-    <title> Report </title>
+    <title> R & R | Report </title>
     <meta name ="viewport" content="width=devoce-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/reportmainstyles.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <nav>
-        <input type="checkbox" id="check">
-        <label for="check" class="checkbtn">
-            <i class="fas fa-bars"></i>
-        </label>
-        <label class="logo">R&R</label>
-        <ul>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">HOME</a></li>
-        </ul>
-    </nav>
+    <div class="reportmainwrapper">
+        
+        <section class="reportmaincolumns">
 
-    <div class="wrapper">
-        <section class="columns">
-            <div class="column">
-                <img src="./assets/pnplogo.png" style="a" width="250px" height="300px" >
-                <h2>PNP</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <input class="btn2" type="button" name="" value="REPORT INCIDENT">
+            <div class="reportmaincolumn" href="#popobangbang">
+                <img src="./assets/pnpfinallogo.jpg" width="390px" height="300px" class="imgreportmain">
+                <h1 style="margin-left: 10px;" class="reportmainheader">Philippine National Police</h1>
+                <p style="margin: 10px; text-align: justify; text-justify: inter-word; ">Lorem ipsum dolor sit amet, consetn ullamcosfdgggg laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <div class="reportmaincontainer">
+                        <div class="reportmainbtn-holder">
+                        <button id="reportmainbtn" type="button">REPORT</button>
+                        </div>
+                    </div>
             </div>
 
-            <div class="column">
-                <img src="./assets/bfplogo.png"  width="250px" height="300px" >
-                <h2>BFP</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <input class="btn2" type="button" name="" value="REPORT INCIDENT">
+            <div class="reportmaincolumn" href="#">
+                <img src="./assets/bfpfinallogo.jpg"  width="390px" height="300px" class="imgreportmain">
+                <h1 style="margin-left: 10px;" class="reportmainheader">Bureau of Fire Protection</h1>
+                <p style="margin: 10px; text-align: justify; text-justify: inter-word;">Lorem ipsum dolor sit amet, consetn ullamcosfdgggg laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="reportmaincontainer">
+                        <div class="reportmainbtn-holder">
+                        <button id="reportmainbtn" type="button">REPORT</button>
+                        </div>
+                    </div>
             </div>
 
-            <div class="column">
-                <img src="./assets/baranggaylogo.png" width="250px" height="300px" >
-                <h2>Barangay</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <input class="btn2" type="button" name="" value="REPORT INCIDENT">
+            <div class="reportmaincolumn" href="#">
+                <img src="./assets/barangay2.jpg" width="390px" height="300px" class="imgreportmain">
+                <h1 style="margin-left: 10px;" class="reportmainheader">Local Barangay</h1>
+                <p style="margin: 10px; text-align: justify; text-justify: inter-word;">Lorem ipsum dolor sit amet, consetn ullamcosfdgggg laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="reportmaincontainer">
+                        <div class="reportmainbtn-holder">
+                        <button id="reportmainbtn" type="button">REPORT</button>
+                        </div>
+                    </div>
             </div>
-
 
         </section>
-    </div>
 
-    <div class="footer">
-            <div class="footer-section-about" id="first">
-                <p style="font-family: Courier; font-size: 35px; font-weight: bold;">ABOUT US</p>
-            </div>
-            <div class="footer-section-links" id="second">
-            <ul>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">HOME</a></li>
-            </ul>
-            </div>
-        <div class="footer-bottom">
-            &copy; Copyright © R&R 2021 Digital All Rights Reserved
-        </div>
     </div>
+    
 </body>
 </html>
 
