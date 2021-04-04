9a5150ee6e62dd92639de2bf4cb96789eea63787
[junmar0117] <[marjungil17@gmail.com]>
2021-04-04 13:31:34 +0800

    add.

--- B_profile.php
+++ B_profile.php
@@ -1,4 +1,5 @@
 <?php
+include_once('UserHeader.html');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,23 +8,10 @@
     <title> Home Page </title>
     <meta name ="viewport" content="width=devoce-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/test.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <nav>
-        <input type="checkbox" id="check">
-        <label for="check" class="checkbtn">
-            <i class="fas fa-bars"></i>
-        </label>
-        <label class="logo">R&R</label>
-        <ul>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">HOME</a></li>
-        </ul>
-    </nav>
     <section>
 
     <div class="profileBox">
@@ -61,23 +49,7 @@
     </div>
     </section>
 
-    <div class="footer">
-            <div class="footer-section-about" id="first">
-                <p style="font-family: Courier; font-size: 35px; font-weight: bold;">ABOUT US</p>
-            </div>
-            <div class="footer-section-links" id="second">
-            <ul>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">HOME</a></li>
-            </ul>
-            </div>
-        <div class="footer-bottom">
-            &copy; Copyright © R&R 2021 Digital All Rights Reserved
-        </div>
-    </div>
+    
 </body>
 </html>
 
