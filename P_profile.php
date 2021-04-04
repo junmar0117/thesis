0b2bda6b704aa8983014c5a53a645c78841a2449
dellyy <dell.jardin.maranan@gmail.com>
2021-04-04 19:28:59 +0800

    index.php draft

--- P_profile.php
+++ P_profile.php
@@ -6,7 +6,7 @@
     <meta charset = "utf-8">
     <title> Home Page </title>
     <meta name ="viewport" content="width=devoce-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/style.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
 <body>
