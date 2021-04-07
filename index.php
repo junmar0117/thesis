093fe647b81f56826177204300a3f7187317489f
dellyy <dell.jardin.maranan@gmail.com>
2021-04-07 20:17:54 +0800

    index, styles.css

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset = "utf-8">
     <title> Home Page </title>
     <meta name ="viewport" content="width=devoce-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/styles1.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -15,34 +15,45 @@
 <body>
 
     <nav>
-    <?php
-        include_once('header.html');
-    ?>
     </nav>
     
     <section>
+    <div class="banner">
+        <img class="banner-image" src="./assets/samplebanner1.jpg">
+    </div>
+
     <div class="container-fluid">
         <h1>R & R</h1>
         <p>Incident Reporting Web Application</p>
         <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
         <hr>
         <div class="row">
-            <div class="col-sm-4" style="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet nisl eu diam fermentum molestie. In hac habitasse platea dictumst. Etiam id scelerisque nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras blandit mi ut nisi pretium luctus. Proin suscipit nisi vitae nisl molestie aliquet sed ut nunc. Mauris turpis libero, tincidunt in ligula eget, molestie molestie odio. Integer vitae lorem eros. Donec gravida auctor libero, vel vestibulum ipsum laoreet in. Integer posuere nisl id dui vestibulum, ut volutpat diam interdum. Phasellus malesuada mattis odio, eu aliquet diam imperdiet a. Cras auctor orci eu libero sagittis, mattis elementum lacus venenatis. Sed efficitur, est ac imperdiet auctor, enim neque mollis tellus, nec tristique mauris metus sit amet ipsum. Ut id vulputate quam.
+            <div class="container p-3 my-3">
+                <h2>SHORT GUIDE ABOUT THE APP</h2>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet nisl eu diam fermentum molestie. In hac habitasse platea dictumst. Etiam id scelerisque nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras blandit mi ut nisi pretium luctus. Proin suscipit nisi vitae nisl molestie aliquet sed ut nunc. Mauris turpis libero, tincidunt in ligula eget, molestie molestie odio. Integer vitae lorem eros. Donec gravida auctor libero, vel vestibulum ipsum laoreet in. Integer posuere nisl id dui vestibulum, ut volutpat diam interdum. Phasellus malesuada mattis odio, eu aliquet diam imperdiet a. Cras auctor orci eu libero sagittis, mattis elementum lacus venenatis. Sed efficitur, est ac imperdiet auctor, enim neque mollis tellus, nec tristique mauris metus sit amet ipsum. Ut id vulputate quam.
 
-            Sed vel finibus metus, non pellentesque urna. Suspendisse fringilla magna eu risus placerat, vel eleifend nisi convallis. Donec nec semper odio. Donec in justo purus. Maecenas ultrices neque ante, a volutpat lectus pretium sed. Nam mi leo, sagittis ut mi a, porta rhoncus nisi. Aliquam nisi felis, faucibus id arcu in, maximus aliquet est. Donec in diam varius lorem elementum mollis sed a est. Nulla magna metus, aliquam in ultricies ac, scelerisque ac eros. Cras dignissim ex risus, ac sagittis libero commodo quis. Donec ut feugiat neque. Mauris eu quam ac nisl accumsan bibendum. Proin semper non lectus a imperdiet.</div>
-            <div class="col-sm-4" style="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet nisl eu diam fermentum molestie. In hac habitasse platea dictumst. Etiam id scelerisque nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras blandit mi ut nisi pretium luctus. Proin suscipit nisi vitae nisl molestie aliquet sed ut nunc. Mauris turpis libero, tincidunt in ligula eget, molestie molestie odio. Integer vitae lorem eros. Donec gravida auctor libero, vel vestibulum ipsum laoreet in. Integer posuere nisl id dui vestibulum, ut volutpat diam interdum. Phasellus malesuada mattis odio, eu aliquet diam imperdiet a. Cras auctor orci eu libero sagittis, mattis elementum lacus venenatis. Sed efficitur, est ac imperdiet auctor, enim neque mollis tellus, nec tristique mauris metus sit amet ipsum. Ut id vulputate quam.
+            
+                Sed vel finibus metus, non pellentesque urna. Suspendisse fringilla magna eu risus placerat, vel eleifend nisi convallis. Donec nec semper odio. Donec in justo purus. Maecenas ultrices neque ante, a volutpat lectus pretium sed. Nam mi leo, sagittis ut mi a, porta rhoncus nisi. Aliquam nisi felis, faucibus id arcu in, maximus aliquet est. Donec in diam varius lorem elementum mollis sed a est. Nulla magna metus, aliquam in ultricies ac, scelerisque ac eros. Cras dignissim ex risus, ac sagittis libero commodo quis. Donec ut feugiat neque. Mauris eu quam ac nisl accumsan bibendum. Proin semper non lectus a imperdiet.</div>
 
-            Sed vel finibus metus, non pellentesque urna. Suspendisse fringilla magna eu risus placerat, vel eleifend nisi convallis. Donec nec semper odio. Donec in justo purus. Maecenas ultrices neque ante, a volutpat lectus pretium sed. Nam mi leo, sagittis ut mi a, porta rhoncus nisi. Aliquam nisi felis, faucibus id arcu in, maximus aliquet est. Donec in diam varius lorem elementum mollis sed a est. Nulla magna metus, aliquam in ultricies ac, scelerisque ac eros. Cras dignissim ex risus, ac sagittis libero commodo quis. Donec ut feugiat neque. Mauris eu quam ac nisl accumsan bibendum. Proin semper non lectus a imperdiet.</div>
-            <div class="col-sm-4" style="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet nisl eu diam fermentum molestie. In hac habitasse platea dictumst. Etiam id scelerisque nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras blandit mi ut nisi pretium luctus. Proin suscipit nisi vitae nisl molestie aliquet sed ut nunc. Mauris turpis libero, tincidunt in ligula eget, molestie molestie odio. Integer vitae lorem eros. Donec gravida auctor libero, vel vestibulum ipsum laoreet in. Integer posuere nisl id dui vestibulum, ut volutpat diam interdum. Phasellus malesuada mattis odio, eu aliquet diam imperdiet a. Cras auctor orci eu libero sagittis, mattis elementum lacus venenatis. Sed efficitur, est ac imperdiet auctor, enim neque mollis tellus, nec tristique mauris metus sit amet ipsum. Ut id vulputate quam.
+            <div class="container p-3 my-3">
+                <h2>PNP, BFP, & BARANGAY</h2>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet nisl eu diam fermentum molestie. In hac habitasse platea dictumst. Etiam id scelerisque nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras blandit mi ut nisi pretium luctus. Proin suscipit nisi vitae nisl molestie aliquet sed ut nunc. Mauris turpis libero, tincidunt in ligula eget, molestie molestie odio. Integer vitae lorem eros. Donec gravida auctor libero, vel vestibulum ipsum laoreet in. Integer posuere nisl id dui vestibulum, ut volutpat diam interdum. Phasellus malesuada mattis odio, eu aliquet diam imperdiet a. Cras auctor orci eu libero sagittis, mattis elementum lacus venenatis. Sed efficitur, est ac imperdiet auctor, enim neque mollis tellus, nec tristique mauris metus sit amet ipsum. Ut id vulputate quam.
 
-            Sed vel finibus metus, non pellentesque urna. Suspendisse fringilla magna eu risus placerat, vel eleifend nisi convallis. Donec nec semper odio. Donec in justo purus. Maecenas ultrices neque ante, a volutpat lectus pretium sed. Nam mi leo, sagittis ut mi a, porta rhoncus nisi. Aliquam nisi felis, faucibus id arcu in, maximus aliquet est. Donec in diam varius lorem elementum mollis sed a est. Nulla magna metus, aliquam in ultricies ac, scelerisque ac eros. Cras dignissim ex risus, ac sagittis libero commodo quis. Donec ut feugiat neque. Mauris eu quam ac nisl accumsan bibendum. Proin semper non lectus a imperdiet.</div>
+            
+                Sed vel finibus metus, non pellentesque urna. Suspendisse fringilla magna eu risus placerat, vel eleifend nisi convallis. Donec nec semper odio. Donec in justo purus. Maecenas ultrices neque ante, a volutpat lectus pretium sed. Nam mi leo, sagittis ut mi a, porta rhoncus nisi. Aliquam nisi felis, faucibus id arcu in, maximus aliquet est. Donec in diam varius lorem elementum mollis sed a est. Nulla magna metus, aliquam in ultricies ac, scelerisque ac eros. Cras dignissim ex risus, ac sagittis libero commodo quis. Donec ut feugiat neque. Mauris eu quam ac nisl accumsan bibendum. Proin semper non lectus a imperdiet.</div>
+            
+            <div class="container p-3 my-3">
+                <h2>ABOUT US</h2>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet nisl eu diam fermentum molestie. In hac habitasse platea dictumst. Etiam id scelerisque nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras blandit mi ut nisi pretium luctus. Proin suscipit nisi vitae nisl molestie aliquet sed ut nunc. Mauris turpis libero, tincidunt in ligula eget, molestie molestie odio. Integer vitae lorem eros. Donec gravida auctor libero, vel vestibulum ipsum laoreet in. Integer posuere nisl id dui vestibulum, ut volutpat diam interdum. Phasellus malesuada mattis odio, eu aliquet diam imperdiet a. Cras auctor orci eu libero sagittis, mattis elementum lacus venenatis. Sed efficitur, est ac imperdiet auctor, enim neque mollis tellus, nec tristique mauris metus sit amet ipsum. Ut id vulputate quam.
+
+            
+                Sed vel finibus metus, non pellentesque urna. Suspendisse fringilla magna eu risus placerat, vel eleifend nisi convallis. Donec nec semper odio. Donec in justo purus. Maecenas ultrices neque ante, a volutpat lectus pretium sed. Nam mi leo, sagittis ut mi a, porta rhoncus nisi. Aliquam nisi felis, faucibus id arcu in, maximus aliquet est. Donec in diam varius lorem elementum mollis sed a est. Nulla magna metus, aliquam in ultricies ac, scelerisque ac eros. Cras dignissim ex risus, ac sagittis libero commodo quis. Donec ut feugiat neque. Mauris eu quam ac nisl accumsan bibendum. Proin semper non lectus a imperdiet.</div>
         </div>
     </div>
     </section>
+<footer>
 
-    <?php
-include_once('footer.html');
-?>
+</footer>
 </body>
 </html>
 
