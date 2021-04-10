ef7c700331adb0073eb8872bbefb5647c12dc820
solarman123 <lrlaurena@gmail.com>
2021-04-10 14:35:28 +0800

    edited index (1st edit), new css for index

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
-<?php
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset = "utf-8">
-    <title> Home Page </title>
+    <title> R & R | Home</title>
     <meta name ="viewport" content="width=devoce-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/styles1.css">
+    <link rel="stylesheet" href="./css/homepage.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
@@ -17,44 +15,38 @@
     <nav>
     </nav>
     
-    <section>
-    <div class="banner">
-        <img class="banner-image" src="./assets/samplebanner1.jpg">
+    <div class="indexBG">
+        <img src="./assets/samplebanner1.jpg" alt="mpbg" style="width:100%">
+        <button class="btn">Report Now</button>
     </div>
 
-    <div class="container-fluid">
-        <h1>R & R</h1>
-        <p>Incident Reporting Web Application</p>
-        <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
-        <hr>
-        <div class="row">
-            <div class="container p-3 my-3">
-                <h2>SHORT GUIDE ABOUT THE APP</h2>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet nisl eu diam fermentum molestie. In hac habitasse platea dictumst. Etiam id scelerisque nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras blandit mi ut nisi pretium luctus. Proin suscipit nisi vitae nisl molestie aliquet sed ut nunc. Mauris turpis libero, tincidunt in ligula eget, molestie molestie odio. Integer vitae lorem eros. Donec gravida auctor libero, vel vestibulum ipsum laoreet in. Integer posuere nisl id dui vestibulum, ut volutpat diam interdum. Phasellus malesuada mattis odio, eu aliquet diam imperdiet a. Cras auctor orci eu libero sagittis, mattis elementum lacus venenatis. Sed efficitur, est ac imperdiet auctor, enim neque mollis tellus, nec tristique mauris metus sit amet ipsum. Ut id vulputate quam.
+    <div class="indexContent">
+        
+            <table class="indexGuide">
+                <tr>
+                    <td><h2>GUIDE</h2></td>
+                    <td><p>Lorem ipsum doncepto libero, tinci</p></td>
+                </tr>
+            </table>
 
+            <table class="indexPBB">
+                <tr>
+                    <td><h2>PNP, BFP, & BARANGAY</h2></td>
+                    <td><p>Lorem ipst. Era, nitalesuada mem nec tristique mauris metus sit amet ipsum. Ut id vulputate quam</p></td>
+                </tr>
+            </table>
             
-                Sed vel finibus metus, non pellentesque urna. Suspendisse fringilla magna eu risus placerat, vel eleifend nisi convallis. Donec nec semper odio. Donec in justo purus. Maecenas ultrices neque ante, a volutpat lectus pretium sed. Nam mi leo, sagittis ut mi a, porta rhoncus nisi. Aliquam nisi felis, faucibus id arcu in, maximus aliquet est. Donec in diam varius lorem elementum mollis sed a est. Nulla magna metus, aliquam in ultricies ac, scelerisque ac eros. Cras dignissim ex risus, ac sagittis libero commodo quis. Donec ut feugiat neque. Mauris eu quam ac nisl accumsan bibendum. Proin semper non lectus a imperdiet.</div>
-
-            <div class="container p-3 my-3">
-                <h2>PNP, BFP, & BARANGAY</h2>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet nisl eu diam fermentum molestie. In hac habitasse platea dictumst. Etiam id scelerisque nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras blandit mi ut nisi pretium luctus. Proin suscipit nisi vitae nisl molestie aliquet sed ut nunc. Mauris turpis libero, tincidunt in ligula eget, molestie molestie odio. Integer vitae lorem eros. Donec gravida auctor libero, vel vestibulum ipsum laoreet in. Integer posuere nisl id dui vestibulum, ut volutpat diam interdum. Phasellus malesuada mattis odio, eu aliquet diam imperdiet a. Cras auctor orci eu libero sagittis, mattis elementum lacus venenatis. Sed efficitur, est ac imperdiet auctor, enim neque mollis tellus, nec tristique mauris metus sit amet ipsum. Ut id vulputate quam.
-
-            
-                Sed vel finibus metus, non pellentesque urna. Suspendisse fringilla magna eu risus placerat, vel eleifend nisi convallis. Donec nec semper odio. Donec in justo purus. Maecenas ultrices neque ante, a volutpat lectus pretium sed. Nam mi leo, sagittis ut mi a, porta rhoncus nisi. Aliquam nisi felis, faucibus id arcu in, maximus aliquet est. Donec in diam varius lorem elementum mollis sed a est. Nulla magna metus, aliquam in ultricies ac, scelerisque ac eros. Cras dignissim ex risus, ac sagittis libero commodo quis. Donec ut feugiat neque. Mauris eu quam ac nisl accumsan bibendum. Proin semper non lectus a imperdiet.</div>
-            
-            <div class="container p-3 my-3">
-                <h2>ABOUT US</h2>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet nisl eu diam fermentum molestie. In hac habitasse platea dictumst. Etiam id scelerisque nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras blandit mi ut nisi pretium luctus. Proin suscipit nisi vitae nisl molestie aliquet sed ut nunc. Mauris turpis libero, tincidunt in ligula eget, molestie molestie odio. Integer vitae lorem eros. Donec gravida auctor libero, vel vestibulum ipsum laoreet in. Integer posuere nisl id dui vestibulum, ut volutpat diam interdum. Phasellus malesuada mattis odio, eu aliquet diam imperdiet a. Cras auctor orci eu libero sagittis, mattis elementum lacus venenatis. Sed efficitur, est ac imperdiet auctor, enim neque mollis tellus, nec tristique mauris metus sit amet ipsum. Ut id vulputate quam.
-
-            
-                Sed vel finibus metus, non pellentesque urna. Suspendisse fringilla magna eu risus placerat, vel eleifend nisi convallis. Donec nec semper odio. Donec in justo purus. Maecenas ultrices neque ante, a volutpat lectus pretium sed. Nam mi leo, sagittis ut mi a, porta rhoncus nisi. Aliquam nisi felis, faucibus id arcu in, maximus aliquet est. Donec in diam varius lorem elementum mollis sed a est. Nulla magna metus, aliquam in ultricies ac, scelerisque ac eros. Cras dignissim ex risus, ac sagittis libero commodo quis. Donec ut feugiat neque. Mauris eu quam ac nisl accumsan bibendum. Proin semper non lectus a imperdiet.</div>
-        </div>
+            <table class="indexAbout">
+                <tr>
+                    <td><h2>ABOUT US</h2></td>
+                    <td><p>Lorem ipa, peripsumis elementum lacus venenatis. Sed efficitur, est ac d vulputate quam.</p></td>
+                </tr>
+            </table>
     </div>
-    </section>
-<footer>
 
-</footer>
 </body>
+<footer>
+</footer>
 </html>
 
 
