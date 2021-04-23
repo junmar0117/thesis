374165a086a723304ec9857c0a0d6929b9bbac2c
[junmar0117] <[marjungil17@gmail.com]>
2021-04-23 22:29:49 +0800

    added login for officials

---
+++ F_login.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset = "utf-8">
+    <title> Login Page </title>
+    <meta name ="viewport" content="width=devoce-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/C_register_login.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    <nav>
+        <?php
+            include_once('header.html');
+        ?>
+    </nav>
+
+    <div class="login-box">
+        <h1>Firefighter User Login</h1>
+        <h2>Input username and password</h2>
+        <form action="B_checklogin.php" method="POST">
+
+        <div class="login-box2">
+            <input type="text" required="required" name="username" placeholder="">
+            <label>Username</label>
+        </div>
+
+        <div class="login-box2">
+            <input type="password" required="required" name="password" placeholder="">
+            <label>Password</label>
+        </div>
+        
+        <input type="submit" value="Login" class="C_loginbtn">
+    </div>
+    <section>
+    
+    </section>
+
+</body>
+</html>
+
+
+
