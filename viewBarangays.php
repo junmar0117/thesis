e5dfe15dd50a6a7c6ffcac4f1d9ff01b706a8d1f
[junmar0117] <[marjungil17@gmail.com]>
2021-05-13 16:09:22 +0800

    barangay

--- viewBarangays.php
+++ viewBarangays.php
@@ -163,7 +163,7 @@ if(mysqli_num_rows($row_b) > 0)
     <?php
     require 'connection.php';  
     $result5=mysqli_query($con,"SELECT count(*) as total5 from reports WHERE barangay='838'");
-    $data5=mysqli_fetch_assoc($result3);
+    $data5=mysqli_fetch_assoc($result5);
     ?>
     <div id="Barangay 838" class="tabcontent">
         <h3>Barangay 838</h3>
@@ -189,7 +189,7 @@ if(mysqli_num_rows($row_b) > 0)
     <?php
     require 'connection.php';  
     $result7=mysqli_query($con,"SELECT count(*) as total7 from reports WHERE barangay='840'");
-    $data7=mysqli_fetch_assoc($result3);
+    $data7=mysqli_fetch_assoc($result7);
     ?>
     <div id="Barangay 840" class="tabcontent">
         <h3>Barangay 840</h3>
@@ -221,7 +221,7 @@ if(mysqli_num_rows($row_b) > 0)
         <h3>Barangay 842</h3>
         <p>Address:West Zandra Street Corner President Quirino, 842 Pandacan, Manila, 1011 Metro Manila</p>
         <p>Contact Number: (02) 8255 2113</p>
-        <p>Number of Reported Incidents: <?php echo $data['total10'] ?></p>
+        <p>Number of Reported Incidents: <?php echo $data10['total10'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.6084006475507!2d121.00003565809183!3d14.58669579745285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c992ce77b379%3A0x9bece87521d77d1f!2sBrgy.%20842%2C%20Pandacan%2C%20Manila%2C%201011%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620706867829!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 
@@ -293,7 +293,7 @@ if(mysqli_num_rows($row_b) > 0)
     <?php
     require 'connection.php';  
     $result06=mysqli_query($con,"SELECT count(*) as total06 from reports WHERE barangay='848'");
-    $data306=mysqli_fetch_assoc($result06);
+    $data06=mysqli_fetch_assoc($result06);
     ?>
     <div id="Barangay 848" class="tabcontent">
         <h3>Barangay 848</h3>
@@ -403,7 +403,7 @@ if(mysqli_num_rows($row_b) > 0)
         <h3>Barangay 857</h3>
         <p>Address: 1445 Labores Extension, Barangay 857, Pandacan, Manila, 1000, Metro Manila</p>
         <p>Contact Number: No Data Available</p>
-        <p>Number of Reported Incidents:<?php echo $data004['total004'] ?></p>
+        <p>Number of Reported Incidents: <?php echo $data004['total004'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3861.2040621393076!2d121.00049361492304!3d14.587444989810846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9934917b677%3A0x3323b5862ad9ddb3!2sBrgy.%20857%2C%20Pandacan%2C%20Manila%2C%201011%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620707191458!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 
     </div>
@@ -411,198 +411,198 @@ if(mysqli_num_rows($row_b) > 0)
     <?php
     //LAST
     require 'connection.php';  
-    $result3=mysqli_query($con,"SELECT count(*) as total3 from reports WHERE barangay='858'");
-    $data3=mysqli_fetch_assoc($result3);
+    $result005=mysqli_query($con,"SELECT count(*) as total005 from reports WHERE barangay='858'");
+    $data005=mysqli_fetch_assoc($result005);
     ?>
     <div id="Barangay 858" class="tabcontent">
         <h3>Barangay 858</h3>
         <p>Address: 1324 Librada Avelino Street, Pandacan, Manila, 1000 Metro Manila</p>
         <p>Contact Number: No Data Available</p>
-        <p>Number of Reported Incidents:</p>
+        <p>Number of Reported Incidents: <?php echo $data005['total005'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.612788930778!2d121.00257305809176!3d14.586218147452897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9ec8ce2987d%3A0x16d9a556251d3c23!2sBrgy.%20858%2C%20Pandacan%2C%20Manila%2C%201011%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620707205181!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 
     </div>
 
     <?php
     require 'connection.php';  
-    $result3=mysqli_query($con,"SELECT count(*) as total3 from reports WHERE barangay='859'");
-    $data3=mysqli_fetch_assoc($result3);
+    $result006=mysqli_query($con,"SELECT count(*) as total006 from reports WHERE barangay='859'");
+    $data006=mysqli_fetch_assoc($result006);
     ?>
     <div id="Barangay 859" class="tabcontent">
         <h3>Barangay 859</h3>
         <p>Address: Baeta Street, Barangay 859, Zone 93, Pandacan, Manila, 1000 Metro Manila</p>
         <p>Contact Number: (02) 8564 8063</p>
-        <p>Number of Reported Incidents:</p>
+        <p>Number of Reported Incidents: <?php echo $data006['total006'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.6098751660286!2d121.00317130809178!3d14.586550447452854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c99352d23685%3A0x8a81b1eea724818b!2sBrgy.%20859%2C%20Pandacan%2C%20Manila%2C%201011%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620707230635!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 
     <?php
     require 'connection.php';  
-    $result3=mysqli_query($con,"SELECT count(*) as total3 from reports WHERE barangay='860'");
-    $data3=mysqli_fetch_assoc($result3);
+    $result007=mysqli_query($con,"SELECT count(*) as total007 from reports WHERE barangay='860'");
+    $data007=mysqli_fetch_assoc($result3);
     ?>
     <div id="Barangay 860" class="tabcontent">
         <h3>Barangay 860</h3>
         <p>Address: Gen. Osmalik St, Barangay 860, Manila, 1011, Philippines</p>
         <p>Contact Number: No Data Available</p>
-        <p>Number of Reported Incidents:</p>
+        <p>Number of Reported Incidents: <?php echo $data007['total007'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.6098751660286!2d121.00317130809178!3d14.586550447452854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9ebaa1de7e7%3A0x4528322861c60ad2!2sBrgy.%20860%2C%20Pandacan%2C%20Manila%2C%201011%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620707244456!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 
     <?php
     require 'connection.php';  
-    $result3=mysqli_query($con,"SELECT count(*) as total3 from reports WHERE barangay='861'");
-    $data3=mysqli_fetch_assoc($result3);
+    $result008=mysqli_query($con,"SELECT count(*) as total008 from reports WHERE barangay='861'");
+    $data008=mysqli_fetch_assoc($result008);
     ?>
     <div id="Barangay 861" class="tabcontent">
         <h3>Barangay 861</h3>
         <p>Address:Barangay 861, Zone 93, Pandacan, Manila, 1000 Metro Manila</p>
         <p>Contact Number: No Data Available</p>
-        <p>Number of Reported Incidents:</p>
+        <p>Number of Reported Incidents: <?php echo $data008['total008'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.575593000584!2d121.00602510809182!3d14.590459597452227!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9ebcf1096b1%3A0x9d2bd7d6fbe1551f!2sBrgy.%20861%2C%20Pandacan%2C%20Manila%2C%201011%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620707259399!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 
     </div>
 
     <?php
     require 'connection.php';  
-    $result3=mysqli_query($con,"SELECT count(*) as total3 from reports WHERE barangay='862'");
-    $data3=mysqli_fetch_assoc($result3);
+    $result009=mysqli_query($con,"SELECT count(*) as total009 from reports WHERE barangay='862'");
+    $data009=mysqli_fetch_assoc($result009);
     ?>
     <div id="Barangay 862" class="tabcontent">
         <h3>Barangay 862</h3>
         <p>Address:  Zone 93, Adolfo, Pandacan, Manila, 1000 Metro Manila</p>
         <p>Contact Number: (02) 8589 0876</p>
-        <p>Number of Reported Incidents:</p>
+        <p>Number of Reported Incidents: <?php echo $data009['total009'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.596012630594!2d121.00446395809183!3d14.588131297452597!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9eb6f7ded95%3A0xbccc7b97ff517606!2sBrgy.%20862%2C%20Pandacan%2C%20Manila%2C%201011%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620707332769!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 
     <?php
     require 'connection.php';  
-    $result3=mysqli_query($con,"SELECT count(*) as total3 from reports WHERE barangay='863'");
-    $data3=mysqli_fetch_assoc($result3);
+    $result0010=mysqli_query($con,"SELECT count(*) as total0010 from reports WHERE barangay='863'");
+    $data0010=mysqli_fetch_assoc($result0010);
     ?>
     <div id="Barangay 863" class="tabcontent">
         <h3>Barangay 863</h3>
         <p>Address: Zone 93 Cisne, Pandacan, Manila, 1000 Metro Manila</p>
         <p>Contact Number: (02) 8564 8355</p>
-        <p>Number of Reported Incidents:</p>
+        <p>Number of Reported Incidents: <?php echo $data0010['total0010'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.596012630594!2d121.00446395809183!3d14.588131297452597!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9eb87352995%3A0x86c54dc8aa750597!2sBrgy.%20863%2C%20Pandacan%2C%20Manila%2C%201011%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620707343386!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 
     <?php
     require 'connection.php';  
-    $result3=mysqli_query($con,"SELECT count(*) as total3 from reports WHERE barangay='864'");
-    $data3=mysqli_fetch_assoc($result3);
+    $result0001=mysqli_query($con,"SELECT count(*) as total0001 from reports WHERE barangay='864'");
+    $data0001=mysqli_fetch_assoc($result0001);
     ?>
     <div id="Barangay 864" class="tabcontent">
         <h3>Barangay 864</h3>
         <p>Address: Zone 93, 1000 Francisco Balagtas, Pandacan, Manila, 1011 Metro Manilaa</p>
         <p>Contact Number: (02) 8562 3290</p>
-        <p>Number of Reported Incidents:</p>
+        <p>Number of Reported Incidents: <?php echo $data0001['total0001'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3861.170127027249!2d121.00275006492319!3d14.589379789809646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9eca8fe3c75%3A0x690d1fa055718ee3!2sBrgy.%20864%2C%20Pandacan%2C%20Manila%2C%201011%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620707354858!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 
     <?php
     require 'connection.php';  
-    $result3=mysqli_query($con,"SELECT count(*) as total3 from reports WHERE barangay='865'");
-    $data3=mysqli_fetch_assoc($result3);
+    $result0002=mysqli_query($con,"SELECT count(*) as total0002 from reports WHERE barangay='865'");
+    $data0002=mysqli_fetch_assoc($result0002);
     ?>
     <div id="Barangay 865" class="tabcontent">
         <h3>Barangay 865</h3>
         <p>Address:Barangay 865, Zone 93, Pandacan, Manila, Metro Manila</p>
         <p>Contact Number: 0920 483 6149</p>
-        <p>Number of Reported Incidents:</p>
+        <p>Number of Reported Incidents: <?php echo $data0002['total0002'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3861.211876167848!2d121.00245506492332!3d14.586999439811134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9ea66ca3c4f%3A0xe8832f07404929a2!2sBrgy.%20865%2C%20Pandacan%2C%20Manila%2C%201011%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620707368424!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 
     <?php
     require 'connection.php';  
-    $result3=mysqli_query($con,"SELECT count(*) as total3 from reports WHERE barangay='866'");
-    $data3=mysqli_fetch_assoc($result3);
+    $result0003=mysqli_query($con,"SELECT count(*) as total0003 from reports WHERE barangay='866'");
+    $data0003=mysqli_fetch_assoc($result3);
     ?>
     <div id="Barangay 866" class="tabcontent">
         <h3>Barangay 866</h3>
         <p>Address:2099-C Felix Street, Pandacan, Manila, Metro Manila</p>
         <p>Contact Number: 523-7005</p>
-        <p>Number of Reported Incidents:</p>
+        <p>Number of Reported Incidents: <?php echo $data0003['total0003'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3861.2760561861414!2d121.00485571484009!3d14.583339439813471!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c99457685507%3A0x52a255337c44e24d!2sBrgy.%20866%2C%20Pandacan%2C%20Manila%2C%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620712543396!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
 
         <?php
     require 'connection.php';  
-    $result3=mysqli_query($con,"SELECT count(*) as total3 from reports WHERE barangay='867'");
-    $data3=mysqli_fetch_assoc($result3);
+    $result0004=mysqli_query($con,"SELECT count(*) as total0004 from reports WHERE barangay='867'");
+    $data0004=mysqli_fetch_assoc($result0004);
     ?>
     <div id="Barangay 867" class="tabcontent">
         <h3>Barangay 867</h3>
         <p>Address: 1011 Rosal, Pandacan, Manila, 1011 Metro Manila</p>
         <p>Contact Number: (02) 8589 2794</p>
-        <p>Number of Reported Incidents:</p>
+        <p>Number of Reported Incidents: <?php echo $data0004['total0004'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.6320652286881!2d121.00415015809176!3d14.584019597453265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c99486da1b31%3A0xd5fd36bac4b1659e!2sBrgy.%20867%2C%20Pandacan%2C%20Manila%2C%201011%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620707475220!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
 
     <?php
     require 'connection.php';  
-    $result3=mysqli_query($con,"SELECT count(*) as total3 from reports WHERE barangay='868'");
-    $data3=mysqli_fetch_assoc($result3);
+    $result0005=mysqli_query($con,"SELECT count(*) as total0005 from reports WHERE barangay='868'");
+    $data0005=mysqli_fetch_assoc($result0005);
     ?>
     <div id="Barangay 868" class="tabcontent">
         <h3>Barangay 868</h3>
         <p>Address: Tent Along Railway Kahilom 2, Barangay 868, Pandacan, Manila, 1000 Metro Manila</p>
         <p>Contact Number: No Data Available</p>
-        <p>Number of Reported Incidents:</p>
+        <p>Number of Reported Incidents: <?php echo $data0005['total0005'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.6300172213894!2d121.0025167080597!3d14.584253197453243!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9937453ebd5%3A0x28ae3affc424178!2sBrgy.%20868%2C%20Pandacan%2C%20Manila%2C%201011%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620707501148!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 
     <?php
     require 'connection.php';  
-    $result3=mysqli_query($con,"SELECT count(*) as total3 from reports WHERE barangay='869'");
-    $data3=mysqli_fetch_assoc($result3);
+    $result0006=mysqli_query($con,"SELECT count(*) as total0006 from reports WHERE barangay='869'");
+    $data0006=mysqli_fetch_assoc($result0006);
     ?>
     <div id="Barangay 869" class="tabcontent">
         <h3>Barangay 869</h3>
         <p>Address: 1845 Int. 12 Bo. Kapampangan, Pandacan, Manila, 1000 Metro Manila</p>
         <p>Contact Number: (02) 8588 2530</p>
-        <p>Number of Reported Incidents:</p>
+        <p>Number of Reported Incidents: <?php echo $data0006['total0006'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.6300172213894!2d121.0025167080597!3d14.584253197453243!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c996ac293a67%3A0xea7354ff70b97ddc!2sBrgy.%20869%2C%20Pandacan%2C%20Manila%2C%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620707513866!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 
     <?php
     require 'connection.php';  
-    $result3=mysqli_query($con,"SELECT count(*) as total3 from reports WHERE barangay='870'");
-    $data3=mysqli_fetch_assoc($result3);
+    $result0007=mysqli_query($con,"SELECT count(*) as total0007 from reports WHERE barangay='870'");
+    $data0007=mysqli_fetch_assoc($result0007);
     ?>
     <div id="Barangay 870" class="tabcontent">
         <h3>Barangay 870</h3>
         <p>Address: Kahilum II Street, Barangay 870, Zone 95, Pandacan, Manila, 1000 Metro Manila</p>
         <p>Contact Number: (02) 8589 9972</p>
-        <p>Number of Reported Incidents:</p>
+        <p>Number of Reported Incidents:  <?php echo $data0007['total0007'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.660282116265!2d121.00408310809182!3d14.580800747453763!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c99380804949%3A0x16bcfdd13f5d6c6f!2sBrgy.%20870%2C%20Pandacan%2C%20Manila%2C%201011%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620707580083!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 
     <?php
     require 'connection.php';  
-    $result3=mysqli_query($con,"SELECT count(*) as total3 from reports WHERE barangay='871'");
-    $data3=mysqli_fetch_assoc($result3);
+    $result0008=mysqli_query($con,"SELECT count(*) as total0008 from reports WHERE barangay='871'");
+    $data0008=mysqli_fetch_assoc($result0008);
     ?>
     <div id="Barangay 871" class="tabcontent">
         <h3>Barangay 871</h3>
         <p>Address: 1803 Acacia Street Kahilom I, Barangay 871, Pandacan, Manila, 1004 Metro Manila</p>
         <p>Contact Number: 09753009033</p>
-        <p>Number of Reported Incidents:</p>
+        <p>Number of Reported Incidents:  <?php echo $data0008['total0008'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.6346374663615!2d121.00373720809178!3d14.583726197453295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c99392b316c1%3A0xa5592ef179768a21!2sBrgy.%20871%2C%20Pandacan%2C%20Manila%2C%201011%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620707589789!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 
     <?php
     require 'connection.php';  
-    $result3=mysqli_query($con,"SELECT count(*) as total3 from reports WHERE barangay='872'");
-    $data3=mysqli_fetch_assoc($result3);
+    $result0009=mysqli_query($con,"SELECT count(*) as total0009 from reports WHERE barangay='872'");
+    $data0009=mysqli_fetch_assoc($result0009);
     ?>
     <div id="Barangay 872" class="tabcontent">
         <h3>Barangay 872</h3>
         <p>Address:1731 Macopa Street Kahilom I, Pandacan, Manila, 1000 Metro Manila</p>
         <p>Contact Number: 226-8613</p>
-        <p>Number of Reported Incidents: <?php echo $data['total'] ?></p>
+        <p>Number of Reported Incidents: <?php echo $data0009['total0009'] ?></p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.6410998367096!2d121.00144385805964!3d14.582989047453426!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c993b7ce58bf%3A0x32d5ccfe4b774f6d!2sBrgy.%20872%2C%20Pandacan%2C%20Manila%2C%201011%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1620707601849!5m2!1sen!2sph" width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe> 
     </div>
 </div>
