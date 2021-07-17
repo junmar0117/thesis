41e429f785a38004f5fe015fa8a2eb5a8b1ace14
dellyy <dell.jardin.maranan@gmail.com>
2021-07-17 16:41:43 +0800

    monitoring

--- B_monitor.php
+++ B_monitor.php
@@ -110,9 +110,125 @@ include_once('B_Userheader.html');
     </div>
 
     <div id="em" class="tabcontent" style="overflow-x:auto;">
+    <table class="adminMonitorRep">
+            <tr>
+                <th>Report ID</th>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Date Created</th>
+                <th>Concern</th>
+                <th>Status</th>
+                <th>Safe Count</th>
+                <th>View Report</th>
+                <th>View Record</th>
+            </tr>
+            <?php
+            require 'connection.php';   
+            $query = mysqli_query($con, "SELECT * from reports where incident = 'Barangay'"); // SQL Query
+            while($row = mysqli_fetch_array($query))
+            {
+                $r_id = $row['id'];   
+            }
+
+            $querySafe = mysqli_query($con, "SELECT * from saferecords where report_id = $r_id and `safe` = 'Yes'");
+            $safeCount = array();
+            while($row = mysqli_fetch_array($querySafe))
+            {
+                $safeCount[] = $row['safe'];
+            } 
+            $numSafeCount = count($safeCount);        
+
+            $query = mysqli_query($con, "SELECT * from reports where incident = 'Barangay' AND `emergency` = 'Yes' ORDER BY id DESC"); // SQL Query
+            while($row = mysqli_fetch_array($query))
+            {                   
+            ?>
+            
+                <tr>
+                <td><?php echo $row['id']  ?></td>
+                <td><?php echo $row['name']  ?></td>
+                <td><?php echo $row['username']  ?></td>
+                <td><?php echo $row['date']; echo " - "; echo $row['time']?></td>
+                <td><?php echo $row['incident']  ?></td>
+                <td><?php echo $row['status']?></td>
+                <td><?php echo $numSafeCount?></td>
+                <td>
+                    <form action="viewReports.php" method="POST">
+                        <input type="hidden" name="id" value="<?php echo $row['id']?>">
+                        <button type="submit" class="viewReportbtn">View</button>
+                    </form>
+                </td>
+                <td>
+                    <form action="viewRecords.php" method="POST">
+                        <input type="hidden" name="report_id" value="<?php echo $row['id']?>">
+                        <button type="submit" class="viewReportbtn">View</button>
+                    </form>
+                </td>
+                </tr>
+            <?php
+        }
+        ?>
+        </table>
     </div>
 
     <div id="nonem" class="tabcontent" style="overflow-x:auto;">
+    <table class="adminMonitorRep">
+            <tr>
+                <th>Report ID</th>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Date Created</th>
+                <th>Concern</th>
+                <th>Status</th>
+                <th>Safe Count</th>
+                <th>View Report</th>
+                <th>View Record</th>
+            </tr>
+            <?php
+            require 'connection.php';   
+            $query = mysqli_query($con, "SELECT * from reports where incident = 'Barangay'"); // SQL Query
+            while($row = mysqli_fetch_array($query))
+            {
+                $r_id = $row['id'];   
+            }
+
+            $querySafe = mysqli_query($con, "SELECT * from saferecords where report_id = $r_id and `safe` = 'Yes'");
+            $safeCount = array();
+            while($row = mysqli_fetch_array($querySafe))
+            {
+                $safeCount[] = $row['safe'];
+            } 
+            $numSafeCount = count($safeCount);        
+
+            $query = mysqli_query($con, "SELECT * from reports where incident = 'Barangay' AND `emergency` = 'No' ORDER BY id DESC"); // SQL Query
+            while($row = mysqli_fetch_array($query))
+            {                   
+            ?>
+            
+                <tr>
+                <td><?php echo $row['id']  ?></td>
+                <td><?php echo $row['name']  ?></td>
+                <td><?php echo $row['username']  ?></td>
+                <td><?php echo $row['date']; echo " - "; echo $row['time']?></td>
+                <td><?php echo $row['incident']  ?></td>
+                <td><?php echo $row['status']?></td>
+                <td><?php echo $numSafeCount?></td>
+                <td>
+                    <form action="viewReports.php" method="POST">
+                        <input type="hidden" name="id" value="<?php echo $row['id']?>">
+                        <button type="submit" class="viewReportbtn">View</button>
+                    </form>
+                </td>
+                <td>
+                    <form action="viewRecords.php" method="POST">
+                        <input type="hidden" name="report_id" value="<?php echo $row['id']?>">
+                        <button type="submit" class="viewReportbtn">View</button>
+                    </form>
+                </td>
+                </tr>
+            <?php
+        }
+        ?>
+        </table>
     </div>
 
     <script>
