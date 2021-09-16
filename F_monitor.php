6207361551c5ababadfae88a688b980d1ccad3a3
dellyy <dell.jardin.maranan@gmail.com>
2021-09-16 12:48:44 +0800

    f_monitor

--- F_monitor.php
+++ F_monitor.php
@@ -64,7 +64,7 @@ $user = $_SESSION['user']; //assigns user value
             $query = mysqli_query($con, "SELECT * from reports where incident = 'Barangay'"); // SQL Query
             while($row = mysqli_fetch_array($query))
             {
-                $r_id = $row['id'];   
+                $r_id = $row['report_id'];   
             }
 
             $querySafe = mysqli_query($con, "SELECT * from saferecords where report_id = $r_id and `safe` = 'Yes'");
@@ -76,27 +76,27 @@ $user = $_SESSION['user']; //assigns user value
             $numSafeCount = count($safeCount);      
 
             require 'connection.php';    
-            $query = mysqli_query($con, "SELECT * from reports where incident = 'Fire' ORDER BY id DESC"); // SQL Query
+            $query = mysqli_query($con, "SELECT * from reports where incident = 'Fire' ORDER BY report_id DESC"); // SQL Query
             while($row = mysqli_fetch_array($query))
             {
             ?>
                 <tr>
-                <td><?php echo $row['id']  ?></td>
-                <td><?php echo $row['name']  ?></td>
-                <td><?php echo $row['username']  ?></td>
+                <td><?php echo $row['report_id']  ?></td>
+                <td><?php echo $row['names']  ?></td>
+                <td><?php echo $row['usernames']  ?></td>
                 <td><?php echo $row['date']; echo " - "; echo $row['time']?></td>
                 <td><?php echo $row['incident']  ?></td>
                 <td><?php echo $row['status']?></td>
                 <td><?php echo $numSafeCount?></td>
                 <td>
                     <form action="viewReports.php" method="POST">
-                        <input type="hidden" name="id" value="<?php echo $row['id']?>">
+                        <input type="hidden" name="id" value="<?php echo $row['report_id']?>">
                         <button type="submit" class="viewReportbtn2">View</button>
                     </form>
                 </td>
                 <td>
                     <form action="viewRecords.php" method="POST">
-                        <input type="hidden" name="report_id" value="<?php echo $row['id']?>">
+                        <input type="hidden" name="report_id" value="<?php echo $row['report_id']?>">
                         <button type="submit" class="viewReportbtn2">View</button>
                     </form>
                 </td>
