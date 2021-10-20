476875182d37db4acc9f64ffddc08734e8c92817
solarman123 <lrlaurena@gmail.com>
2021-10-20 15:58:18 +0800

    subok modal sa B at F

--- viewReports.php
+++ viewReports.php
@@ -229,7 +229,19 @@ if(mysqli_num_rows($row_b) > 0)
             <h3><?php echo "Proof of Incident"?></h3>
             <table class="viewReportsTable">
                 <tr>
-                <td><a href='<?php echo 'reportFIles/'.$row['file'];?>' target="_blank">View Image</a></td>
+                <td><a id="myBtn">View Image</a></td>
+                <!-- Trigger/Open The Modal -->
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <?php echo 'reportFIles/'.$row['file'];?>
+  </div>
+
+</div>
             </tr>
             </table>
             
@@ -264,6 +276,32 @@ if(mysqli_num_rows($row_b) > 0)
                 });
             }
             google.maps.event.addDomListener(window, "load", initialize());
+
+             // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
             </script>
 
             
@@ -403,7 +441,20 @@ if(mysqli_num_rows($row_b) > 0)
             <h3><?php echo "Proof of Incident"?></h3>
             <table class="viewReportsTable">
                 <tr>
-                <td><a href='<?php echo 'reportFIles/'.$row['file'];?>' target="_blank">View Image</a></td>
+                <td><a id="myBtn">View Image</a></td>
+                <!-- Trigger/Open The Modal -->
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <?php echo 'reportFIles/'.$row['file'];?>
+  </div>
+
+</div>
+
             </tr>
             </table>
             
@@ -439,6 +490,33 @@ if(mysqli_num_rows($row_b) > 0)
                 });
             }
             google.maps.event.addDomListener(window, "load", initialize());
+
+
+            // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
             </script> 
 
 
